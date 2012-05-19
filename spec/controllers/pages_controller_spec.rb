97dfab327fb88c527cecd009e99e8b4a2c8f6308
Wesley Ellis <tahnok@gmail.com>
2012-05-18 21:47:17 -0400

    added a help page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,6 +4,10 @@ describe PagesController do
 
   render_views
 
+  before :each do
+    @base_title = "Trainer | "
+  end
+
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
@@ -12,7 +16,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_selector "title", :content => "Trainer | Home"
+      response.should have_selector "title", :content => @base_title + "Home"
     end
   end
 
@@ -24,7 +28,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-      response.should have_selector "title", :content => "Trainer | Contact"
+      response.should have_selector "title", :content => @base_title + "Contact"
     end
   end
 
@@ -36,10 +40,20 @@ describe PagesController do
 
     it "should have the right title" do
       get 'about'
-      response.should have_selector "title", :content => "Trainer | About"
+      response.should have_selector "title", :content => @base_title + "About"
     end
   end
 
+  describe "Get 'help'" do
+    it "should be succesful" do
+      get 'help'
+      response.should be_success
+    end
 
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector "title", :content => @base_title + "Help"
+    end
+  end
 
 end
